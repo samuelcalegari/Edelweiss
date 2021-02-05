d7f5ef06324c2779bea92f0c48ee4ecd1fc6354a
miro <samuel.calegari@univ-perp.fr>
2021-02-05 18:25:43 +0100

    initial

--- EdelweissApp/urls.py
+++ EdelweissApp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.FrontEnd.index, name='index'),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/hello/', views.HelloView.as_view(), name='hello'),
 ]
--- EdelweissApp/views.py
+++ EdelweissApp/views.py
@@ -3,6 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-def index(request):
-    return render(request, 'index.html', {})
+class FrontEnd():
+
+    def index(request):
+        return render(request, 'index.html', {})
+
+
+class HelloView(APIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
